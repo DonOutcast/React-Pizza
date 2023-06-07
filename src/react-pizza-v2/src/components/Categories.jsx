40e73d8784104578010b57f632873928e75bdb25
DonOutcast <sham1996@yandex.ru>
2023-06-07 08:59:57 +0300

    rendering categories

--- src/react-pizza-v2/src/components/Categories.jsx
+++ src/react-pizza-v2/src/components/Categories.jsx
@@ -3,16 +3,28 @@ import React from 'react';
 
 function Categories() {
     const [activeIndex, setActiveIndex] = React.useState(0);
+    const onClickCategory = (index) => {
+      setActiveIndex(index);
+    }
+
+    const categories = [
+      'Все',
+      'Мясные',
+      'Вегетарианская',
+      'Гриль',
+      'Острые',
+      'Закрытые',
+    ];
 
     return (
       <div className="categories">
       <ul>
-        <li className="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {
+        categories.map((value, i) => (
+          <li onClick={() => onClickCategory(i)} className={activeIndex === i ? "active" : ''}>{value}</li> 
+          )
+        )
+        };
       </ul>
     </div>
     )
