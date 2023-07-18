e6b1857fa1ea24edbbafe85f6ec8c4cd27ba1edd
DonOutcast <sham1996@yandex.ru>
2023-07-18 08:56:34 +0300

    lesson 10 end

--- src/react-pizza-v2/src/pages/Home.jsx
+++ src/react-pizza-v2/src/pages/Home.jsx
@@ -4,12 +4,14 @@ import Categories from "../components/Categories";
 import Sort from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/skeleton";
+import Pagination from "../components/Pagination";
 
 const Home = ({ searchValue }) => {
-      // https://64983dcb9543ce0f49e1ca37.mockapi.io/items
+  // https://64983dcb9543ce0f49e1ca37.mockapi.io/items
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
   const [categoyId, setCategoryId] = React.useState(0);
+  const [currentPage, setCurrentPage] = React.useState(0);
   const [sortType, setSortType] = React.useState(
     {
       name: 'популярности',
@@ -18,8 +20,13 @@ const Home = ({ searchValue }) => {
   );
   
   React.useEffect(() => {
-    setIsLoading(false);
-    fetch(`https://64983dcb9543ce0f49e1ca37.mockapi.io/items?${categoyId > 0 ? `category=${categoyId}` : '' }&sortBy=${sortType.sortProperty}&order=desc`)
+    setIsLoading(true);
+    const category = categoyId > 0 ? `category=${categoyId}` : '';
+    const sortBy = sortType.sortProperty;
+    const search = searchValue ? `&search=${searchValue}` : '';
+
+
+    fetch(`https://64983dcb9543ce0f49e1ca37.mockapi.io/items?page=${currentPage}&limit=4&${category}${search}&sortBy=${sortBy}&order=desc`)
     .then(
       (response) => {
         return response.json(); 
@@ -29,15 +36,9 @@ const Home = ({ searchValue }) => {
       setIsLoading(false);
     });
     window.scrollTo(0, 0);
-    }, [categoyId, sortType]);
+    }, [categoyId, sortType, searchValue, currentPage]);
 
-    const pizzas = items.filter(
-      (obj) => {
-        if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
-          return true;
-        }
-         return false;
-      }).map((obj) => <PizzaBlock key={obj.id} {...obj}/>); 
+    const pizzas = items.map((obj) => <PizzaBlock key={obj.id} {...obj}/>); 
 
     const skeletons =  [...new Array(6)].map((_, index) => <Skeleton key={index}/>);
 
@@ -48,12 +49,9 @@ const Home = ({ searchValue }) => {
                 <Sort value={sortType} onClickSort={(i) => setSortType(i)} />
             </div>
             <h2 className="content__title">Все пиццы</h2>
-            <div className="content__items">
-                {
-                isLoading ? skeletons: pizzas
-            }
-            </div>
-            </div>
+            <div className="content__items">{isLoading ? skeletons: pizzas}</div>
+            <Pagination onChangePage={(number) => setCurrentPage(number)}/>
+          </div>
 
     );
 };
